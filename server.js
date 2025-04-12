a0b737beb1d84df5beb3790f79849f1fe6b1191b
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2025-04-12 10:38:15 -0400

    simuling http method GET/POST using httpie

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 import http from "node:http";
 
+//GET users => buscar usuários
+//POST users => criar usuários
+
 const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  if (method === "GET" && url === "/users") {
+    return res.end("Listagem de usuários");
+  }
+  if (method === "POST" && url === "/users") {
+    return res.end("Criação de usuários");
+  }
   return res.end("Hello ignite");
 });
 
