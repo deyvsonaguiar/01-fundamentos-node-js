d2c5cbf81fcd0a183a0331d5d3c1e229ea19d30b
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2025-04-14 18:57:57 -0400

    Route regex parameters to Delete

--- routes.js
+++ routes.js
@@ -1,3 +1,4 @@
+import { buildRoutePath } from "./build-route-path.js";
 import { Database } from "./database.js";
 import { randomUUID } from "node:crypto";
 
@@ -6,7 +7,7 @@ const database = new Database();
 export const routes = [
   {
     method: "GET",
-    path: "/users",
+    path: buildRoutePath("/users"),
     handler: (req, res) => {
       const users = database.select("users");
       return res.end(JSON.stringify(users));
@@ -14,7 +15,7 @@ export const routes = [
   },
   {
     method: "POST",
-    path: "/users",
+    path: buildRoutePath("/users"),
     handler: (req, res) => {
       const { name, email } = req.body;
 
@@ -27,6 +28,13 @@ export const routes = [
       database.insert("users", users);
 
       return res.writeHead(201).end();
+    },
+  },
+  {
+    method: "DELETE",
+    path: buildRoutePath("/users/:id"),
+    handler: (req, res) => {
+      return res.end();
     },
   },
 ];
