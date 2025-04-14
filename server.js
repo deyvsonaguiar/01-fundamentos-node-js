e4cb157f27d62200117387328ac47526345f95ae
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2025-04-14 17:43:47 -0400

    implemmenting middleware

--- server.js
+++ server.js
@@ -1,31 +1,18 @@
 import http from "node:http";
-import { Database } from "./database.js";
-import { randomUUID } from "node:crypto";
+import { routes } from "./routes.js";
+import { json } from "./middlewares.js";
 
-//GET users => buscar usuários
-//POST users => criar usuários
-
-const database = new Database();
-
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
-  if (method === "GET" && url === "/users") {
-    const users = database.select("users");
+  await json(req, res);
 
-    return res.end(JSON.stringify(users));
-  }
+  const route = routes.find((route) => {
+    return route.method === method && route.path === url;
+  });
 
-  if (method === "POST" && url === "/users") {
-    const users = {
-      id: randomUUID(),
-      name: "Deyvson",
-      email: "deyvsonaguiar@gmail.com",
-    };
-
-    database.insert("users", users);
-
-    return res.writeHead(201).end();
+  if (route) {
+    return route.handler(req, res);
   }
 
   return res.writeHead(404).end("This route don't exists!");
